53cb141e57a144953bb6812c4fcf90237d0b8a8a
Flavio Nogueira <flavionogueirabarros@gmail.com>
2025-03-21 12:03:51 -0300

    finish challenge

--- index.js
+++ index.js
@@ -2,15 +2,34 @@
 //PASSO 2 verificar se o numero repetido é igual aos outros
 //PASSO 3 excluir o numero repetido
 //PASSO 4 retornar os numeros não repetidos e dizer quantos foram deletados
-var numbers = [8, 7, 55, 1, 3, 1, 8, 5, 66, 7, 66, 1300, 8, 5];
+var numbers = [8, 7, 55, 1, 3, 1, 8, 8, 66, 7, 7, 13, 8, 5, 7, 7, 7, 7, 7, 7, 7, 5];
 var deletedCount = 0;
 numbers.sort(function (a, b) { return a - b; });
 var i;
+var repeatedIndexsArray = [];
 for (i = 0; numbers.length > i; ++i) {
-    if (numbers[i] == numbers[i + 1]) {
-        numbers.splice(i, 1);
-        deletedCount++;
+    console.log(numbers);
+    console.log("inicio do loop " + i);
+    for (var j = 0; numbers.length > j; ++j) {
+        if (numbers[i] == numbers[j] && i != j) {
+            console.log("numero repetido: " + numbers[i] + " == " + numbers[j] + " index: " + j);
+            numbers.splice(j, 1);
+            if (numbers[j] == numbers[j - 1]) {
+                j--; //se o numero deletado for igual ao anterior, decrementa o index para não pular o proximo numero
+            }
+            console.log(numbers);
+            deletedCount++;
+        }
+        else {
+            if (i == j) {
+                console.log("é o mesmo numero, na mesma posição " + numbers[i] + " == " + numbers[j] + " no index: " + j);
+            }
+            else {
+                console.log("não é repetido " + numbers[i] + " != " + numbers[j] + " no index: " + j);
+            }
+        }
     }
+    console.log("fim do loop " + i);
 }
 console.log(numbers);
 console.log("numeros deletados:", deletedCount);
